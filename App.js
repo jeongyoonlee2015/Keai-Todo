3e1a0031dbc3a8f0ca2867e061baf5038259a88e
JeongyoonLee <43804152+jeongyoonlee2015@users.noreply.github.com>
2019-04-01 12:32:59 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions } from 'react-native';
+import { Styles, StyleSheet, Text, View, StatusBar, TextInput, Dimensions } from 'react-native';
 
-const{height, width} = Dimensions.get{"window"};
+const{height, width} = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
@@ -9,18 +9,16 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Text sytle={styles.tritle}>Keai To Do</Text>
-        <view style={styles.card>
+        <View style={styles.card}>
           <TextInput style={styles.input} placeholder={"New To Do"} />
           </View>
       </View>
     );
-  }
-
-
+//여기 있던 중괄호 없애버림
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F23657',
+    backgroundColor: '#f26f69',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -34,7 +32,7 @@ const styles = StyleSheet.create({
   card:{
     backgroundColor: "white",
     flex: 1,
-    width: width - 25
+    width: width - 25,
     borderRadius: 10
   }
 });
