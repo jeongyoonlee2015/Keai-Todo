28e961529e8e60700f1846345a9f1706d33fe4b1
JeongyoonLee <43804152+jeongyoonlee2015@users.noreply.github.com>
2019-04-02 11:53:46 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Styles, StyleSheet, Text, View, StatusBar, TextInput, Dimensions } from 'react-native';
+import { Styles, StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
 
 const{height, width} = Dimensions.get("window");
 
@@ -8,12 +8,14 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
-        <Text sytle={styles.tritle}>Keai To Do</Text>
+        <Text sytle={styles.title}>Keai To Do</Text>
         <View style={styles.card}>
           <TextInput style={styles.input} placeholder={"New To Do"} />
           </View>
       </View>
     );
+  }
+}  
 //여기 있던 중괄호 없애버림
 const styles = StyleSheet.create({
   container: {
@@ -24,15 +26,31 @@ const styles = StyleSheet.create({
   },
   title: {
     color: "white",
-    fontSize: 30,
-    marginTop: 50,
+    fontSize: 50,
+    marginTop: 200,
     fontWeight: "200",
-    marginBottom: 30
+    marginBottom: 300
   },
   card:{
     backgroundColor: "white",
     flex: 1,
-    width: width - 25,
-    borderRadius: 10
+    width: width - 100,
+    borderRadius: 10,
+    ...Platform.select({
+      ios
+      : {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius:5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+
+      }
+    })
   }
 });
