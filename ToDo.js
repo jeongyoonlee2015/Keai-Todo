999aa20ac728c3fa60e9648180e9d9c3e4fff824
jeongyoonlee2015 <happyjoy2005@gamil.com>
2019-04-03 15:39:08 +0900

    Append for scroll

---
+++ ToDo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import{View, Text, TouchableOpacity, StyleSheet} from "react-native"
+class ToDo extends React.Component{
+    render(){
+        //stateful component: 수정하면 바뀌어야해서?!
+        return(
+            <View>
+                <Text> Hello I'm a Todo </Text>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({})
