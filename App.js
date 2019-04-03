c9c4b7712490c90e0d8631ebf58e6b8b73c71e22
JeongyoonLee <43804152+jeongyoonlee2015@users.noreply.github.com>
2019-04-03 15:36:45 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Styles, StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+import { Styles, StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
+import ToDo from "./ToDo";
 
 const{height, width} = Dimensions.get("window");
 
@@ -10,30 +11,31 @@ export default class App extends React.Component {
   render() {
     const{newToDo} = this.state;
     return (
-      <View style = {styles.container}>
-        <StatusBar barStyle="light-content" />
-        <Text sytle = {styles.title}>Keai To Do</Text>
-        <View style = {styles.card}>
-          <TextInput style={styles.input} 
-                     placeholder={"New To Do"} 
-                     value={newToDo} 
-                     onChangeText={this._controlNewTodo}
-                     placeholderTextColor={"#999"}
-                     returnKeyType={"done"}
-                  autoCorrect={false}
-          />
-        <scrollView> //NewFile
-        </scrollView>
+        <View style = {styles.container}>
+          <StatusBar barStyle="light-content" />
+          <Text sytle = {styles.title, {fontSize: 300}}>Keai To Do</Text> //fontSize안바뀜
+          <View style = {styles.card}>
+            <TextInput style={styles.input}
+                       placeholder={"New To Do"}
+                       value={newToDo}
+                       onChangeText={this._controlNewTodo}
+                       placeholderTextColor={"#999"}
+                       returnKeyType={"done"}
+                       autoCorrect={false}
+            />
+            <scrollView> //NewFile
+              <ToDo />
+            </scrollView>
           </View>
-      </View>
+        </View>
     );
   }
   _controlNewToDo = text => {
     this.setState({
       newToDo: text
     });
-  }
-}  
+  };
+}
 //여기 있던 중괄호 없애버림
 const styles = StyleSheet.create({
   container: {
@@ -44,10 +46,9 @@ const styles = StyleSheet.create({
   },
   title: {
     color: "white",
-    fontSize: 500,
-    marginTop: 300,
-    fontWeight: "200",
-    marginBottom: 300
+    marginTop: 3,
+    fontWeight: "60",
+    marginBottom: 30
   },
   card:{
     backgroundColor: "white",
@@ -57,7 +58,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     ...Platform.select({
       ios
-      : {
+          : {
         shadowColor: "rgb(50, 50, 50)",
         shadowOpacity: 0.5,
         shadowRadius:5,
